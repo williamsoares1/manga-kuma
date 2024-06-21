64b320d260248af2d4a8d6360ce1dc271fe1ad2e
Cristiano Gonçalves <cristianog.contato@gmail.com>
2024-06-21 20:13:07 -0300

    informacoes pagina usuario

--- project/src/context/AuthContext.tsx
+++ project/src/context/AuthContext.tsx
@@ -1,19 +1,24 @@
-import { ReactNode, createContext, useState } from 'react';
+import { ReactNode, createContext, useState } from "react";
 
 interface AuthType {
   user: boolean;
+  userData: {};
   handleLoginSuccess: () => void;
   handleLogout: () => void;
+  handleUserData: (nome: string, email: string) => void;
 }
 
 export const AuthContext = createContext<AuthType>({
   user: false,
+  userData: {},
   handleLoginSuccess: () => {},
   handleLogout: () => {},
+  handleUserData: () => {},
 });
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState(false);
+  const [userData, setUserData] = useState({});
 
   const handleLoginSuccess = () => {
     setUser(true);
@@ -23,9 +28,21 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     setUser(false);
   };
 
+  const handleUserData = (nome: string, email: string) => {
+    setUserData({ nome, email });
+  };
+
   return (
-    <AuthContext.Provider value={{ user, handleLoginSuccess, handleLogout }}>
-      { children }
+    <AuthContext.Provider
+      value={{
+        user,
+        userData,
+        handleLoginSuccess,
+        handleLogout,
+        handleUserData,
+      }}
+    >
+      {children}
     </AuthContext.Provider>
   );
 };
